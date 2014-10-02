abe264edac9b3101290d3789d52a091c19498097
unknown <wilso199@EAS-BEN006-C6.it.muohio.edu>
2014-10-02 12:52:07 -0400

    Updated to make changes presented in class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include "hashPrimes.h"
-#include "HashTable.h"
 #include <string>
+
+unsigned long hash(std::string k);
+
+#include "HashTable.h"
 #include <iostream>
 #include <sstream>
 
@@ -15,7 +18,7 @@
 unsigned long hash(std::string k){
   unsigned long m = hashPrimes[NUM_HASH_PRIMES-2];
   unsigned long ret = 0;
-  for(int i=0;i<k.size();i++){
+  for(unsigned int i=0;i<k.size();i++){
     ret = (256*ret + k[i])%m;
   }
   return ret;
@@ -162,7 +165,7 @@ void testGrow(){
     return;
   }
 
-  for(int j=0; j<=i;j++){
+  for(unsigned int j=0; j<=i;j++){
     std::ostringstream ss;
     ss << j;
     if(!testHash.keyExists(ss.str())){
